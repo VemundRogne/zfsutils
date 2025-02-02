347c24d21e4d88eba47160ac088c60dfb3f37739
Vemund <vemundrogne@gmail.com>
2025-02-02 16:05:48 +0100

    feat: two snapshots, some data in the second snapshot

--- zfsutils/test/test_create_snapshots.cpp
+++ zfsutils/test/test_create_snapshots.cpp
@@ -1,28 +1,39 @@
 #include "core.hpp"
 #include "libzfs.h"
 #include <exception>
+#include <filesystem>
+#include <fstream>
 
 int main() {
     try {
         zfs::Pool testpool_A = zfs::ZFS::getPoolByName("zfsutils_testpool_A");
-
         zfs::Dataset testDataset = testpool_A.openDataset("testDataset");
-        std::cout << "testDataset zfs name: " << testDataset.name()
-                  << std::endl;
+
+        // Now we want to make a snapshot _before_ doing anything on the
+        // dataset. Then we want to 'put' something in the dataset, and then
+        // make another snapshot
+
+        testDataset.createSnapshot("initialSnapshot");
 
         try {
-            zfs::Snapshot snap = testDataset.createSnapshot("Hello");
-        } catch (std::exception &e) {
-            std::cout << e.what() << std::endl;
+            std::ofstream outFile;
+            outFile.exceptions(std::ofstream::badbit | std::ofstream::failbit);
+            outFile.open(testDataset.getMountpoint() + "/testfile.txt");
+            outFile << "Hello, there! This is some text!" << std::endl;
+            outFile.close();
+
+        } catch (const std::ofstream::failure &e) {
+            std::cerr << e.what() << std::endl;
             return -1;
         }
 
-        try {
-            zfs::Snapshot snap = testDataset.openSnapshot("Hello");
-        } catch (std::exception &e) {
-            std::cout << e.what() << std::endl;
+        if (!std::filesystem::exists(testDataset.getMountpoint() +
+                                     "/testfile.txt")) {
+            std::cerr << "Did not manage to make the testfile :(" << std::endl;
             return -1;
         }
+
+        testDataset.createSnapshot("secondSnapshot");
 
     } catch (std::exception &e) {
         std::cout << e.what() << std::endl;
