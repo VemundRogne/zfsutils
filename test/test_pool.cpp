a9be6fa3d53442465399cef81b038eda4484da11
Vemund <vemundrogne@gmail.com>
2025-07-11 16:01:09 +0200

    test: test getPools and verify zfsutils_testpool_A/B are found

--- test/test_pool.cpp
+++ test/test_pool.cpp
@@ -1,5 +1,8 @@
+#include "libzfs.h"
+#include "zfsutils/core.hpp"
 #include "zfsutils/pool.hpp"
 #include <iostream>
+#include <ranges>
 
 void usePool(zfsutils::Pool pool) {
     std::cout << "I have pool: " << pool.name() << std::endl;
@@ -19,9 +22,30 @@ int main() {
         return -1;
     }
 
+    // Try to iterate top-level datasets:
+    zfsutils::internal::IterHelper<zfs_handle_t> iterHelper;
+    iterHelper.callback = [](zfs_handle_t *zh) -> int {
+        std::cout << std::string{zfs_get_name(zh)} << std::endl;
+        return 0;
+    };
+    zfs_iter_root(zfsutils::internal::LibzfsHandle::Handle(),
+                  iterHelper.zfs_callback, &iterHelper);
+
     auto pools = zfsutils::Pool::getPools();
-    std::cout << "Got " << pools.size() << " pools:" << std::endl;
-    for (auto &pool : pools) {
-        std::cout << "  " << pool.name() << std::endl;
+
+    auto pool_in_vector = [](const std::vector<zfsutils::Pool> &pools,
+                             const std::string &name) {
+        return std::find_if(pools.begin(), pools.end(),
+                            [&](const zfsutils::Pool &pool) {
+                                return pool.name() == name;
+                            }) != pools.end();
+    };
+
+    if (pool_in_vector(pools, "zfsutils_testpool_A") != true) {
+        throw std::runtime_error{"zfsutils_testpool_A not found :("};
+    }
+
+    if (pool_in_vector(pools, "zfsutils_testpool_B") != true) {
+        throw std::runtime_error{"zfsutils_testpool_B not found :("};
     }
 }
